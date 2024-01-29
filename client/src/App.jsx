c7328ceac960daf12becddf6229d4395e26610b3
Michael Lyons <michael.lyons1112@gmail.com>
2024-01-29 19:07:36 +0000

    Making red marks go away...

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,11 @@
 import React, { useEffect, useState } from 'react';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Title from './Title';
-import Layout from "./pages/Layout";
-import Home from "./pages/Home";
-import NewPage from './NewPage';
 
 function App() {
 
   return (
     <div>
       <Title />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Home />} />
-            <Route path="NewPage" element={<NewPage />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
     </div>
   )
 }
