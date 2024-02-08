5737a91ecdeca8f0c8ac676c16750aee90908497
Michael Lyons <michael.lyons1112@gmail.com>
2024-02-08 17:47:42 +0000

    Testing .env and .gitignore are working

--- server/main.js
+++ server/main.js
@@ -1,8 +1,19 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
 
 const app = express();
 const port = 5000;
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "Data",
+  password: process.env[Database_Password],
+  port: 5432
+  });
+
+db.connect();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
