710f6ef58ebf01158e0c1bb458956cb42db629a4
Michael Lyons <michael.lyons1112@gmail.com>
2024-02-12 16:14:00 +0000

    Adding functional answer recognition

--- client/src/Choice.jsx
+++ client/src/Choice.jsx
@@ -2,12 +2,11 @@ import React, { useState } from "react";
 import Button from '@mui/material/Button';
 
 function Choices(props) {
-  const correctAnswer = {backgroundColor: "green"};
-  const falseAnswer = {backgroundColor: "red"};
+  const trueOrFalse = props.correct ? {backgroundColor: "green"} : {backgroundColor: "red"};
 
   return (
           <div className="choice">
-              <Button onClick={props.handleClick} variant="contained" className="option-button" index={props.index} correct={props.correct} style={props.showAnswer ? correctAnswer : null }>
+              <Button onClick={props.handleClick} variant="contained" className="option-button" index={props.index} correct={props.correct} style={props.showAnswer ? trueOrFalse : null }>
                 {props.property}
               </Button>
           </div>
